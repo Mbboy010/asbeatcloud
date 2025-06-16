6da51e1a5fae0e249df187133e9d239b73013721
mbboy010 <mbboy010@gmail.com>
2025-06-16 07:25:02 +0100

    Initial commit

--- src/app/(Instrumental)/beats/page.tsx
+++ src/app/(Instrumental)/beats/page.tsx
@@ -1,38 +1,7 @@
-"use client"
+import React from 'react'
 
-import { useEffect, useState } from 'react';
-import { account,storage } from '@/lib/appwrite';
-
-export default function UserIdComponent() {
-  const [userId, setUserId] = useState(null);
-
-  useEffect(() => {
-    account.get()
-      .then((user) => {
-        setUserId(user.$id);
-      })
-      .catch((error) => {
-        console.error('User not authenticated:', error);
-        setUserId(null);
-      });
-  });
-  
-  
-
-async function deleteFile() {
-  try {
-    await storage.deleteFile("6849a34c0027417cde77", "684ea7a5000ca181e8cf");
-    console.log("✅ File deleted successfully.");
-  } catch (error) {
-    console.error("❌ Error deleting file:", error.message);
-  }
-}
-
-
-
-  return ( <div>
-     <p className="mt-20 text-white
-  ">User ID: {userId}</p>
-  <button  className="text-white p-4" onClick={deleteFile}>save data</button>
-  </div>)
+export default function BeatPage() {
+  return (
+    <div></div>
+  )
 }
