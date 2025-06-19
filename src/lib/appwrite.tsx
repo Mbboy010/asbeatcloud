2a04970785098a5667a9422309f6115d59d5f5d9
mbboy010 <mbboy010@gmail.com>
2025-06-19 09:10:52 +0100

    Initial commit

--- src/lib/appwrite.tsx
+++ src/lib/appwrite.tsx
@@ -2,9 +2,17 @@ import { Client, Account, Databases, Storage, ID } from 'appwrite';
 
 const client = new Client();
 
-client
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
+const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
+const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
+
+if (!endpoint) {
+  throw new Error('NEXT_PUBLIC_APPWRITE_ENDPOINT is not defined in environment variables');
+}
+if (!projectId) {
+  throw new Error('NEXT_PUBLIC_APPWRITE_PROJECT_ID is not defined in environment variables');
+}
+
+client.setEndpoint(endpoint).setProject(projectId);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
